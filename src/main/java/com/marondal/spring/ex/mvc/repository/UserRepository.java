e7e48bb587121993fdf79069b3d372d7ed1249cb
dulumary <lecture@hagulu.com>
2024-11-06 18:49:13 +0900

    [김인규]  - mvc 사이클 예제

---
+++ src/main/java/com/marondal/spring/ex/mvc/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.marondal.spring.ex.mvc.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.marondal.spring.ex.mvc.domain.User;
+
+@Mapper
+public interface UserRepository {
+	
+	public int insertUser(
+			@Param("name") String name
+			, @Param("birthday") String birthday
+			, @Param("email") String email
+			, @Param("introduce") String introduce);
+	
+	public User selectLastUser();
+
+}
---
+++ src/main/java/com/marondal/spring/ex/mvc/service/UserService.java
@@ -0,0 +1,40 @@
+package com.marondal.spring.ex.mvc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marondal.spring.ex.mvc.domain.User;
+import com.marondal.spring.ex.mvc.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public int addUser(
+			String name
+			, String birthday
+			, String email
+			, String introduce) {
+		
+		int count = userRepository.insertUser(name, birthday, email, introduce);
+		
+		return count;
+		
+	}
+	
+	public User getLastUser() {
+		User user = userRepository.selectLastUser();
+		
+		return user;
+	}
+
+}
+
+
+
+
+
+
+
